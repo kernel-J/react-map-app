b05896175af5ac151638a7260e253fe51a600bda
kernel_j <jenniferwong89@gmail.com>
2019-04-30 11:52:19 +0200

    add google maps and markers

---
+++ src/Components/GoogleMap.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+  withScriptjs,
+  withGoogleMap,
+  GoogleMap,
+  Marker
+} from 'react-google-maps';
+import PropTypes from 'prop-types';
+
+import { google } from '../config.json';
+
+const { defaultLocation, zoom } = google;
+
+const GoogleMapWrapper = withScriptjs(withGoogleMap(props =>
+  <GoogleMap
+    defaultZoom={zoom}
+    defaultCenter={{ lat: defaultLocation.lat, lng: defaultLocation.lng }}
+  >
+    {props.cities.map(city => {
+      if (city.selected) {
+        return (
+          <Marker
+            key={city.id}
+            position={{ lat: city.latitude, lng: city.longitude }}
+            animation={1}
+          />
+        );
+      }
+      return (
+        <Marker
+          key={city.id}
+          position={{ lat: city.latitude, lng: city.longitude }}
+        />
+      );
+    })}
+  </GoogleMap>
+));
+
+GoogleMapWrapper.propTypes = {
+  cities: PropTypes.array
+}
+
+export default GoogleMapWrapper;
--- src/Containers/App.js
+++ src/Containers/App.js
@@ -4,8 +4,11 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 import { fetchCities } from '../Redux/Actions';
+import GoogleMapWrapper from '../Components/GoogleMap';
 import ScrollList from './ScrollList';
+import { google } from '../config.json';
 
+const { api } = google;
 
 const Page = styled.div`
   display: flex;
@@ -18,6 +21,9 @@ const List = styled.div`
   padding: 0.5em;
 `;
 
+const Map = styled.div`
+  width:70%;
+`;
 
 class App extends Component {
   static propTypes = {
@@ -30,23 +36,36 @@ class App extends Component {
   }
 
   render() {
+    const { cities } = this.props;
+
     return (
       <Page>
         <List>
           <ScrollList />
         </List>
+        <Map>
+          <GoogleMapWrapper
+            cities={cities}
+            googleMapURL={api}
+            loadingElement={<div style={{ height: `100%` }} />}
+            containerElement={<div style={{ height: `100%` }} />}
+            mapElement={<div style={{ height: `100%` }} />}
+          />
         </Map>
       </Page>
     );
   };
 };
 
+const mapStateToProps = state => ({
+  cities: state.cities
+});
 
 const mapDispatchToProps = dispatch => ({
   fetchCities: () => dispatch(fetchCities())
 });
 
 export default connect(
-  null,
+  mapStateToProps,
   mapDispatchToProps
 )(App);
